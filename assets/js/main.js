8561e6ca60dd88557c91c70c477dffefa8b4053a
makramfais@gmail.com <makramfais@gmail.com>
2022-08-27 08:24:06 +0700

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,137 @@
+// SHOW MENU
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
+
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show-menu");
+    });
+  }
+};
+showMenu("nav-toggle", "nav-menu");
+
+// REMOVE MENU MOBILE
+const navLink = document.querySelectorAll(".nav__link");
+
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  // When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove("show-menu");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+// SCROLL SECTION ACTIVE LINK
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+}
+window.addEventListener("scroll", scrollActive);
+
+// CHANGE BACKGROUND HEADER
+function scrollHeader() {
+  const nav = document.getElementById("header");
+  // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+  if (this.scrollY >= 200) nav.classList.add("scroll-header");
+  else nav.classList.remove("scroll-header");
+}
+window.addEventListener("scroll", scrollHeader);
+
+// SCROLL TOP
+function scrollTop() {
+  const scrollTop = document.getElementById("scroll-top");
+  // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if (this.scrollY >= 560) scrollTop.classList.add("show-scroll");
+  else scrollTop.classList.remove("scroll-top");
+}
+window.addEventListener("scroll", scrollTop);
+
+// MIXTITUP PORTFOLIO
+const mixer = mixitup(".portfolio__container", {
+  selectors: {
+    target: ".portfolio__content",
+  },
+  animation: {
+    duration: 400,
+  },
+});
+
+// link active portfolio
+const linkPortfolio = document.querySelectorAll(".portfolio__item");
+
+function activePortfolio() {
+  if (linkPortfolio) {
+    linkPortfolio.forEach((l) => l.classList.remove(".active-portfolio"));
+    this.classList.add("active-portfolio");
+  }
+}
+
+linkPortfolio.forEach((l) => l.addEventListener("click", activePortfolio));
+
+// GSAP ANIMATION
+gsap.from(".home__img", {
+  opacity: 0,
+  duration: 2,
+  delay: 0.5,
+  x: 60,
+});
+
+gsap.from(".home__data", {
+  opacity: 0,
+  duration: 2,
+  delay: 0.8,
+  y: 25,
+});
+
+gsap.from(".home__greeting, .home__name, .home__profession, .home__button", {
+  opacity: 0,
+  duration: 2,
+  delay: 1,
+  y: 25,
+  ease: "expo.out",
+  stagger: 0.2,
+});
+
+gsap.from(".nav__logo, .nav__toggle", {
+  opacity: 0,
+  duration: 2,
+  delay: 1.5,
+  y: 25,
+  ease: "expo.out",
+  stagger: 0.2,
+});
+
+gsap.from(".nav__item", {
+  opacity: 0,
+  duration: 2,
+  delay: 1.8,
+  y: 25,
+  ease: "expo.out",
+  stagger: 0.2,
+});
+
+gsap.from(".home__social-icon", {
+  opacity: 0,
+  duration: 2,
+  delay: 1.8,
+  y: 2.3,
+  ease: "expo.out",
+  stagger: 0.2,
+});
